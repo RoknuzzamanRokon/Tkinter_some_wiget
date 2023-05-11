f5e0371a0d072c24c2a2e0f24efb5247a8501adc
Rokon Ron <rokon.raz@gmail.com>
2023-05-11 15:57:57 +0600

    Using astric and pandas moduel for sum function

---
+++ example.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def add(*args):
+    x = []
+    for i in args:
+        x.append(i)
+    pandas_dataframe = pd.DataFrame(x)
+
+    sum = pandas_dataframe.sum()[0]
+    return sum
+
+
+print(add(3,5,7,8))
+
+#
+# def add(*args):
+#     sum = 0
+#     for x in args:
+#         sum = sum + x
+#     return sum
+#
+#
+# print(add(3, 5, 8))
+
