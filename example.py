2d09c6da23e770bc3326ed588776e98220a5efcf
Rokon Ron <rokon.raz@gmail.com>
2023-05-11 16:07:29 +0600

    Creating def function and assasign the '**' kye

--- example.py
+++ example.py
@@ -1,17 +1,17 @@
-import pandas as pd
-
-
-def add(*args):
-    x = []
-    for i in args:
-        x.append(i)
-    pandas_dataframe = pd.DataFrame(x)
-
-    sum = pandas_dataframe.sum()[0]
-    return sum
-
-
-print(add(3,5,7,8))
+# import pandas as pd
+#
+#
+# def add(*args):
+#     x = []
+#     for i in args:
+#         x.append(i)
+#     pandas_dataframe = pd.DataFrame(x)
+#
+#     sum = pandas_dataframe.sum()[0]
+#     return sum
+#
+#
+# print(add(3,5,7,8))
 
 #
 # def add(*args):
@@ -23,3 +23,14 @@ print(add(3,5,7,8))
 #
 # print(add(3, 5, 8))
 
+
+def add(**strick):
+    print(strick)
+
+    for kye, value in strick.items():
+        print(kye)
+        print(value)
+
+
+
+add(add=5, multiply=3)
