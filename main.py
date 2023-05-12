fbc5cc776f45883968999deaa998cc4cc8fe0723
Rokon Ron <rokon.raz@gmail.com>
2023-05-12 20:20:52 +0600

    tkinter widget example

--- main.py
+++ main.py
@@ -4,25 +4,38 @@ window = tkinter.Tk()
 window.title("This is my tkinter window")
 window.minsize(width=500, height=300)
 
-# my_button = tkinter.Button(text='Play Game', font=("Arial", 24, "bold"))
-# my_button.config(bg="red", font=("Arial", 20))
-# my_button.pack()
+
+# Label
 
 my_label = tkinter.Label(text='click', font=("Arial", 24, "bold"))
 # my_label.config() # Create a custom size for label.
 my_label.pack(side='top')
+my_label.config(text="Here config my label text", foreground="Green", )
 
+
+# Button
 
 def button_fun():
-    my_label["text"] = "This is edited text"
-
-    print("I Got click ")
-
-
+    my_label["text"] = input.get()
+    my_label["bg"] = "Green"
+    my_label["foreground"] = "black"
 
 
-my_button = tkinter.Button(text='Click me', command=button_fun)
+
+
+my_button = tkinter.Button(text='Click me', command=button_fun, width=20, height=1)
+tkinter.Frame(master=my_button, background="Yellow")
+my_button.config(text="start Game")
 my_button.pack()
 
+# my_button = tkinter.Button(text='Play Game', font=("Arial", 24, "bold"))
+# my_button.config(bg="red", font=("Arial", 20))
+# my_button.pack()
+
+
+input = tkinter.Entry(width=20)
+input.pack()
+
+print(input.get())
 window.mainloop()
 
---
+++ widget_exmple.py
@@ -0,0 +1,70 @@
+import tkinter
+
+
+# Create a window.
+window = tkinter.Tk()
+window.title("Widget Example")
+window.minsize(width=700, height=600)
+
+
+# Create Label
+my_label = tkinter.Label(text="This is new text", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+# Create Button
+my_button = tkinter.Button()
+my_button.config(text="Click Here")
+my_button.pack()
+
+
+# Create Entry
+my_entry = tkinter.Entry()
+my_entry.config(borderwidth=5, width=30)
+my_entry.pack()
+
+
+# Create Text Box
+my_textbox = tkinter.Text()
+my_textbox.config(height=10, width=30)
+my_textbox.pack()
+
+
+# Create Spinbox
+my_spinbox = tkinter.Spinbox()
+my_spinbox.pack()
+
+
+# Create Scale
+my_scale = tkinter.Scale()
+my_scale.pack()
+
+
+# Create checkbutton
+my_checkbutton = tkinter.Checkbutton()
+my_checkbutton.config(text="Is On")
+my_checkbutton.pack()
+
+
+# create Radiobutton
+my_radiobutton = tkinter.Radiobutton()
+my_radiobutton.config(text="Option")
+my_radiobutton.pack()
+
+
+# Create Listbox
+my_listbox = tkinter.Listbox()
+my_listbox.config()
+my_listbox.pack()
+
+
+
+
+
+
+
+
+
+window.mainloop()
+
+
