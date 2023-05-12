d9d4a7e24c172a6f6163e5d34281439c2e29b518
Rokon Ron <rokon.raz@gmail.com>
2023-05-12 07:23:30 +0600

    Create a button and use command key for button

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ window.minsize(width=500, height=300)
 # my_button.config(bg="red", font=("Arial", 20))
 # my_button.pack()
 
-my_label = tkinter.Label(text='I am label', font=("Arial", 24, "bold"), )
+my_label = tkinter.Label(text='click', font=("Arial", 24, "bold"))
 # my_label.config() # Create a custom size for label.
 my_label.pack(side='top')
 
 
-my_label02 = tkinter.Label(text='label', font=("Arial", 20,), )
-# my_label.config() # Create a custom size for label.
-my_label02.pack(side='left')
+def button_fun():
+    my_label["text"] = "This is edited text"
+
+    print("I Got click ")
+
+
+
+
+my_button = tkinter.Button(text='Click me', command=button_fun)
+my_button.pack()
 
 window.mainloop()
 
